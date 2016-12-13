0ea43fc7732043724bf3910c8228b869065244f1
nexus <nexus@localhost>
2016-12-13 19:24:06 +0200

    bugfixing

---
+++ grid/GridView.php
@@ -0,0 +1,14 @@
+<?php
+namespace gvasilopulos\enhancedgii\grid;
+//use kartik\datecontrol\DateControl;
+
+class GridView extends \kartik\grid\GridView
+{
+    const FILTER_DATECONTROL = '\kartik\datecontrol\DateControl';
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
